091ae5ce462457ac68cd2b323938505f258819aa
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-18 11:33:01 -0500

    inital commit + comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect, } from 'react';
 
 function App() {
 
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     fetchFeedback();
@@ -33,12 +33,42 @@ const dispatch = useDispatch();
   };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+      </div>
+
+      {/* LINKS TO BE CREATED */}
+      {/* 
+
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        
+        <Route path="/feedback-form" exact>
+          <FeedbackForm />
+          </Route>
+
+        <Route path="/feedback-form-page-1" exact>
+          <FeedbackForm />
+          </Route>
+
+        <Route path="/feedback-form-page-2" exact>
+          <FeedbackForm />
+          </Route>
+
+        <Route path="/feedback-form-page-2" exact>
+          <FeedbackForm />
+          </Route>
+
+
+      */ }
+    </Router>
+
   );
 }
 
